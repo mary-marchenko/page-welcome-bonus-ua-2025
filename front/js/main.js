6c9380ddb6153a3c2f5902665b579d25c7336dff
Mari <mari@MacBook-Pro.local>
2025-03-20 13:33:14 +0200

    first desk

---
+++ front/js/main.js
@@ -0,0 +1,68 @@
+(function () {
+    var url = new URL(window.location.href);
+    var params = ['l', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'param1', 'param2', 'param3', 'param4', 'creative_type', 'creative_id'];
+    var linkParams = ['affid', 'cpaid']; // ищем в url redirectUrl в url:
+
+    if (url.searchParams.has('redirectUrl')) {
+        var redirectUrl = new URL(url.searchParams.get('redirectUrl'));
+
+        if (redirectUrl.href.match(/\//g).length === 4 && redirectUrl.searchParams.get('l')) {
+            //если ссылка в ссылка redirectUrl корректная
+            localStorage.setItem('redirectUrl', redirectUrl.href); // указываем точкой входа домен с протоколом из redirectUrl
+        }
+    }
+
+    params.forEach(function (param) {
+        if (url.searchParams.has(param)) localStorage.setItem(param, url.searchParams.get(param));
+    });
+
+    linkParams.forEach(function (linkParam) {
+        if (url.searchParams.has(linkParam)) localStorage.setItem(linkParam, url.searchParams.get(linkParam));
+    });
+
+    window.addEventListener('click', function (e) {
+        var link,
+            parent = e.target.closest('a');
+
+        if (parent.getAttribute('href') !== 'https://tds.favbet.hr') {
+            return;
+        }
+
+        if (parent) {
+            e.preventDefault();
+            var affid = localStorage.getItem('affid');
+            var cpaid = localStorage.getItem('cpaid');
+
+            if (localStorage.getItem("redirectUrl")) {
+                link = new URL(localStorage.getItem("redirectUrl"));
+            } else {
+                link = new URL(parent.href);
+                if (affid && cpaid) {
+                    link.pathname = '/' + affid + '/' + cpaid;
+                }
+            }
+
+            params.forEach(function (param) {
+                if (url.searchParams.has(param)) {
+                    link.searchParams.set(param, localStorage.getItem(param));
+                }
+            });
+
+            document.location.href = link;
+        }
+    });
+})();
+
+// padding no-ios
+document.addEventListener("DOMContentLoaded", function() {
+    let pageLoveBtn = document.querySelector('.fav-page__main-btn');
+    let pageLoveBlock = document.querySelectorAll('.fav-page__main-info-block');
+
+    if (!/iPad|iPhone|iPod|Mac/.test(navigator.userAgent)) {
+        pageLoveBtn.classList.add('noios-padding');
+
+        pageLoveBlock.forEach(function(pageLoveBlock) {
+            pageLoveBlock.classList.add('noios-padding');
+        });
+    }
+});
